a52e59968b215af39795d9dd2d97e4f8a479d895
Pratik Prajapati <pratikprajapati8733@gmail.com>
2024-03-04 12:36:10 +0530

    added Pressable Event

--- App.tsx
+++ App.tsx
@@ -1,28 +1,25 @@
 import React, { Component, useEffect, useState } from 'react';
-import { Text, View, TextInput, Button, FlatList, StyleSheet, ScrollView, SectionList, useWindowDimensions, TouchableHighlight, TouchableOpacity, ActivityIndicator, Platform, Modal, StatusBar } from 'react-native';
+import { Text, View, TextInput, Button, FlatList, StyleSheet, ScrollView, SectionList, useWindowDimensions, TouchableHighlight, TouchableOpacity, ActivityIndicator, Platform, Modal, StatusBar, Pressable } from 'react-native';
 import style from './styles/style';
-import SplashScreen from 'react-native-splash-screen';
+
 
 const App = () => {
   
-  const [hidden , setHiddenStatus] = useState(true);
-  const [changeText , setText] = useState('Statusbar Show');
-  useEffect(()=>{
-     if(hidden == true){
-       setText('Statusbar show');
-     }
-     else{
-       setText('Statusbar hide');
-     }
-  },[hidden])
-  return(
-       <View style={style.main}>
-        <StatusBar hidden={hidden} backgroundColor={'green'}></StatusBar>
-        <View style={style.centerView}>
-        <Button title={changeText} onPress={()=> setHiddenStatus(!hidden)}/>
-        </View>
-       </View>     
-  )
+    return(
+        <View>
+             <Pressable 
+              onPress={() => console.warn("onPress Event Occured")}
+              onLongPress={()=> console.warn("LongPress Event Occured")}
+              onPressIn={() => console.warn("onPressIn Event Occured")}
+              onPressOut={() => console.warn("onPressOut Event Occured")}
+              >
+                   <Text style={style.btn}>
+                        Pressable Event
+                   </Text>
+             </Pressable>
+        </View> 
+    )  
+ 
 }
 
 
