5f06ace487badd490ed421e0ab178e9386857fa0
Pratik Prajapati <pratikprajapati8733@gmail.com>
2024-03-04 13:10:09 +0530

    webview added

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import React, { Component, useEffect, useState } from 'react';
 import { Text, View, TextInput, Button, FlatList, StyleSheet, ScrollView, SectionList, useWindowDimensions, TouchableHighlight, TouchableOpacity, ActivityIndicator, Platform, Modal, StatusBar, Pressable } from 'react-native';
 import style from './styles/style';
+import WebView from 'react-native-webview';
 
 
 const App = () => {
   
     return(
-        <View>
-             <Text style={style.modalText}>Platform OS is {Platform.OS}</Text>
-        </View> 
+           
+             <View style={{flex:1}}>
+                     <WebView  source={{uri:"https://www.google.co.in/"}} style= {{flex:1}}/>
+                  
+             </View>
+             
+           
+        
     )  
  
 }
