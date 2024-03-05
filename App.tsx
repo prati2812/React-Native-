a7af2660b6a0c1513d0d507e8ebe3b2e16c3e3ec
Pratik Prajapati <pratikprajapati8733@gmail.com>
2024-03-05 12:58:16 +0530

    Top tab navigation added

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Text, View, StyleSheet, Button, TextInput} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 const App = () => {
   
