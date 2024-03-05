9a0f69a8ff16b3a848c2aaecd2f43eb7331062c7
Pratik Prajapati <pratikprajapati8733@gmail.com>
2024-03-05 12:48:59 +0530

    file updated

--- App.tsx
+++ App.tsx
@@ -1,22 +1,47 @@
-import React, { Component, useEffect, useState } from 'react';
-import { Text, View, TextInput, Button, FlatList, StyleSheet, ScrollView, SectionList, useWindowDimensions, TouchableHighlight, TouchableOpacity, ActivityIndicator, Platform, Modal, StatusBar, Pressable } from 'react-native';
-import style from './styles/style';
-import WebView from 'react-native-webview';
+import React from 'react';
+import { Text, View, StyleSheet, Button, TextInput} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   
     return(
-           
-             <View style={{flex:1}}>
-                     <WebView  source={{uri:"https://www.google.co.in/"}} style= {{flex:1}}/>
-                  
-             </View>
-             
-           
+             <NavigationContainer>
+                 <Tab.Navigator>
+                     <Tab.Screen component={Home} name='Home'/>
+                     <Tab.Screen component={Add} name='Add' />
+                     <Tab.Screen component={Update} name='Update' />
+                 </Tab.Navigator>
+             </NavigationContainer>
         
     )  
  
+}
+
+const Home = () => {
+  return(
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+           <Text>Home</Text>
+      </View>
+  )
+}
+
+const Add = () => {
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Add</Text>
+    </View>
+  )
+}
+
+const Update = () => {
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Update</Text>
+    </View>
+  )
 }
 
 
