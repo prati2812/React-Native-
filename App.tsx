9b532ac225d86e0bb63022b146e15f7bf78772e2
Pratik Prajapati <pratikprajapati8733@gmail.com>
2024-03-05 18:39:46 +0530

    save data through input field

--- App.tsx
+++ App.tsx
@@ -4,92 +4,36 @@ import style from './styles/style';
 
 
 const App = () => {
+  
+  const [name, setName] = useState('');
+  const [email , setEmail] = useState('');
 
+ const saveData = async () => {
+    const data1 = {"name" : name , "email" : email};
+    const url = 'http://10.0.2.2:3000/users';
+    let response = await fetch(url , {
+       method:'POST',
+       headers:{
+         'Content-type' : 'application/json'
+       },
+       body: JSON.stringify(data1)
+    })
 
-  const  [search , setSearch] = useState('');
-  const [filterdData , setFiteredText] = useState([]);
-  const [data,setData] = useState([]); 
- 
-
-  const searchFilterFunction = (text) =>{
-      if(text){
-         const newData = data.filter((item)=>{
-             const itemData = item.title ? item.title.toUpperCase()
-             : ''.toUpperCase();
-           const textData = text.toUpperCase();
-           return itemData.indexOf(textData) > -1;
-         })
-
-         setFiteredText(newData);
-         setSearch(text);
-      }
-      else{
-         setFiteredText(data);
-         setSearch(text);
-      }   
-  } 
-
-
-  const getData = async () => {
-    try{
-       const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-       const data = await response.json();
-       setData(data);
-    }
-    catch(error){
-      console.warn(error);
-      
-    }
+    response = await response.json();
+    console.warn(response);
+    
  }
 
-
- useEffect(()=> {
-    getData(); 
- }, []);
  
 
     return (
       <View style={{flex: 1}}>
-        <View style= {{marginLeft:10 , marginRight:10 , borderRadius:10}}>
-        <TextInput
-          style={style.textInputStyle}
-          onChangeText={(text) => searchFilterFunction(text)}
-          value={search}
-          underlineColorAndroid="transparent"
-          placeholder="Search Here"
-        />
-        </View>
-        {
-
-
-             filterdData.length  > 0 ?
-             <FlatList
-                 data={filterdData}
-                 renderItem={({item}) => (
-                   <View style={style.modelView}>
-                     <Text style={{fontSize: 20}}>UserId: {item.userId}</Text>
-                     <Text style={{fontSize: 20}}>Id: {item.id}</Text>
-                     <Text style={{fontSize: 20}}>Title:  {item.title}</Text>
-                     <Text style={{fontSize: 20}}>Body:  {item.body}</Text>
-                   </View>
-                 )}
-              /> :
-              <FlatList
-                 data={data}
-                 renderItem={({item}) => (
-                   <View style={style.modelView}>
-                     <Text style={{fontSize: 20}}>UserId: {item.userId}</Text>
-                     <Text style={{fontSize: 20}}>Id: {item.id}</Text>
-                     <Text style={{fontSize: 20}}>Title:  {item.title}</Text>
-                     <Text style={{fontSize: 20}}>Body:  {item.body}</Text>
-                   </View>
-                 )}
-              />
-
-              
-        }
+           <Text style={{textAlign:'center' , fontSize:30}}>Form</Text> 
+           <TextInput placeholder='Enter your name' style={style.item} onChangeText={(text) => setName(text)} value={name}/> 
+           <TextInput placeholder='Enter your email' style={style.item} onChangeText={(text) => setEmail(text)} value={email} />
+           <Button  title='Save Data' onPress={() => saveData()}/>
       </View>
-    );  
+    )  
  
 }
 
