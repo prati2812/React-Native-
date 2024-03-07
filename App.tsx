77b98c28d4f501627bcdb7291368eb2a08e8b34a
Pratik Prajapati <pratikprajapati8733@gmail.com>
2024-03-07 16:51:13 +0530

    authentication through firebase

--- App.tsx
+++ App.tsx
@@ -1,28 +1,60 @@
 import React, { useEffect, useState} from 'react';
-import { Button, Text, View} from 'react-native';
+import { Button, Text, TextInput, View , ToastAndroid} from 'react-native';
 import style from './styles/style';
-import MapView ,{PROVIDER_GOOGLE , Marker} from 'react-native-maps';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import auth from './config/auth';
+
 
 
 const App = () => {
+ 
+   const [email , setEmail] = useState('');
+   const [password , setPassword] = useState(''); 
+
+
+   const login = async () => {
+      let user = await signInWithEmailAndPassword(auth , email , password);
+      if(user){
+          console.warn(user);
+          
+      }
+   }
+
+   const logout = async () => {
+      let logout = await signOut(auth);
+      console.warn(logout);
+      
+   }
 
    return(
       <View style={{flex:1}}>
-          
-           <MapView
-             style={{flex:1}}
-           provider={PROVIDER_GOOGLE} 
-           region={{
-             latitude: 37.78825,
-             longitude: -122.4324,
-             latitudeDelta: 0.015,
-             longitudeDelta: 0.0121,
-           }}
-            >
-              <Marker
-             coordinate={{latitude: 37.78825, longitude: -122.4324}}/>
-           </MapView>
-           
+           <Text style={{margin:10 , textAlign:'center' , fontSize:20, fontWeight:'800', marginTop:40}}>
+               Firebase Authentication
+            </Text>
+
+            <TextInput
+               placeholder='Enter your email'
+               value={email}
+               onChangeText={(text) => setEmail(text)}
+               style={style.item}
+             />
+
+            <TextInput
+               placeholder='Enter your password'
+               value={password}
+               onChangeText={(text) => setPassword(text)}
+               style={style.item}
+             />
+
+            <View style={{marginLeft:20, marginRight:20, marginTop:15}}>
+                <Button title='Sign Up' onPress={() => login()}/>
+            </View>    
+
+            <View style={{marginLeft:20, marginRight:20, marginTop:15}}>
+                <Button title='Logout' onPress={() => logout()}/>
+            </View>  
+
+         
       </View>
    )
 
