101cf41da466172481d628a60bba8d08dceff7d6
Pratik Prajapati <pratikprajapati8733@gmail.com>
2024-03-08 17:49:47 +0530

    alert box

--- App.tsx
+++ App.tsx
@@ -1,41 +1,20 @@
-import React, { useEffect, useState} from 'react';
-import { Button, Text, TextInput, View , ToastAndroid , PermissionsAndroid} from 'react-native';
-import style from './styles/style';
-
-
-
+import React, {useState} from 'react';
+import { View  , Button , Alert} from 'react-native';
 
 const App = () => {
  
+   const alertFunction = () => {
+      Alert.alert(
+         'What is',
+         'Cancel'
+      );
+   } 
 
-   const requestCameraPermission = async () => {
-     
-      
-       const granted = await PermissionsAndroid.requestMultiple(['android.permission.CAMERA' , 
-                         'android.permission.READ_CONTACTS',
-                         'android.permission.READ_SMS']);
-       if(granted[PermissionsAndroid.PERMISSIONS.CAMERA] === PermissionsAndroid.RESULTS.GRANTED && 
-          granted[PermissionsAndroid.PERMISSIONS.READ_CONTACTS] === PermissionsAndroid.RESULTS.GRANTED &&
-          granted[PermissionsAndroid.PERMISSIONS.READ_SMS] === PermissionsAndroid.RESULTS.GRANTED){
-
-            console.log("allowed");
-            
-       }
-       else{
-           
-          console.log("dfgyuh");
-          
-       }                  
-    };
 
    return(
-      <View style={{flex:1}}>
-           <Text style={{margin:10 , textAlign:'center' , fontSize:20, fontWeight:'800', marginTop:40}}>
-               Request Permission
-            </Text>
-
-            <Button title='Ask Permission' onPress={requestCameraPermission}/>         
-      </View>
+        <View>
+               <Button title='Alert' onPress={alertFunction}/>
+        </View>     
    )
 
 }
