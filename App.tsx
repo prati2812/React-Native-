37965735eed1e555fce828aad93fb25d0926c573
Pratik Prajapati <pratikprajapati8733@gmail.com>
2024-03-08 15:11:46 +0530

    runtime permission handling

--- App.tsx
+++ App.tsx
@@ -1,54 +1,36 @@
 import React, { useEffect, useState} from 'react';
-import { Button, Text, TextInput, View , ToastAndroid} from 'react-native';
+import { Button, Text, TextInput, View , ToastAndroid , PermissionsAndroid} from 'react-native';
 import style from './styles/style';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import auth from './config/auth';
+
 
 
 
 const App = () => {
  
-   const [email , setEmail] = useState('');
-   const [password , setPassword] = useState(''); 
 
-
-   const login = async () => {
-      let user = await signInWithEmailAndPassword(auth , email , password);
-      if(user){
-          ToastAndroid.show('Login Successfully' , ToastAndroid.LONG);
-          
+   const requestCameraPermission = async () => {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.CAMERA
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('You can use camera');
+        } else {
+            console.log('permission denied');
+            
+        }
+      } catch (err) {
+        console.warn(err);
       }
-   }
-
-   
+    };
 
    return(
       <View style={{flex:1}}>
            <Text style={{margin:10 , textAlign:'center' , fontSize:20, fontWeight:'800', marginTop:40}}>
-               Firebase Authentication
+               Request Permission
             </Text>
 
-            <TextInput
-               placeholder='Enter your email'
-               value={email}
-               onChangeText={(text) => setEmail(text)}
-               style={style.item}
-             />
-
-            <TextInput
-               placeholder='Enter your password'
-               value={password}
-               onChangeText={(text) => setPassword(text)}
-               style={style.item}
-             />
-
-            <View style={{marginLeft:20, marginRight:20, marginTop:15}}>
-                <Button title='Sign in' onPress={() => login()}/>
-            </View>    
-
-           
-
-         
+            <Button title='Ask Permission' onPress={requestCameraPermission}/>         
       </View>
    )
 
