210fa936a7fe59e6653d73a28a8fc363b570f57b
Pratik Prajapati <pratikprajapati8733@gmail.com>
2024-03-11 18:16:35 +0530

    contact list accessed

--- App.tsx
+++ App.tsx
@@ -1,56 +1,67 @@
-import React, {useEffect, useState} from 'react';
-import { StyleSheet } from 'react-native';
-import { View  , Text, Button} from 'react-native';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { View  , Text,  PermissionsAndroid, Platform} from 'react-native';
+import Contact from 'react-native-contacts';
+
 
 const App = () => {
 
-   const [data, setData] = useState('');
+   const [contacts, setContacts] = useState([]);
 
-   const displayAdvice = async () => {
-      await axios.get('http://api.adviceslip.com/advice/2')
-              .then((res) => {
-                  setData(res.data.slip.advice);
-              })
-              .catch((e) => {
-                  console.log(e);
-              })
+   useEffect(() => {
+     fetchContacts();
+   }, []);
+ 
+   const fetchContacts = async () => {
+     try {
+       if (Platform.OS === 'android') {
+         const granted = await PermissionsAndroid.request(
+           PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+           {
+             title: 'Contacts Permission',
+             message: 'This app needs access to your contacts',
+             buttonNeutral: 'Ask Me Later',
+             buttonNegative: 'Cancel',
+             buttonPositive: 'OK',
+           }
+         );
+         if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+              getContactList(); 
+         } else {
+           console.log('Contacts permission denied');
+         }
+       } 
+     } catch (error) {
+       console.error('Error fetching contacts:', error);
+     }
+   };
+ 
+   
+   const getContactList = () => {
+       Contact.getAll().then((contacts) => {
+             setContacts(contacts);
+             console.warn(contacts);
+             
+       })
+       .catch((e) => {
+           console.log(e);
+           
+       });
    }
 
+ 
+   return (
+     <View>
+       <Text>Contact List:</Text>
+       {contacts.map((contact, index) => (
+         <Text key={index} style={{fontSize:20 , color:'black'}}>{contact?.phoneNumbers[0]?.number}</Text>
+       ))}
+     </View>
+   );
+};  
 
-   return(
-          <View style={styles.container}>
-                <Button  title='Get Advice' onPress={displayAdvice}/>
-                {
-                   data.length ?
-                          <Text>{data}</Text> : null
-                }
-          </View>     
-   )  
 
-}
 
-const styles = StyleSheet.create({ 
-   container: { 
-       flex: 1, 
-       backgroundColor: "#fff", 
-   }, 
-   fabContainer:{
-      justifyContent: "center", 
-      alignItems: "center", 
-      borderRadius: 100, 
-      position: "absolute", 
-      bottom: 70, 
-      right: 40, 
-      backgroundColor: "#26653A", 
-      paddingHorizontal: 20, 
-      paddingVertical: 10, 
-   },
-   fabTxt:{
-      color:'white',
-      fontSize:20
-   }
 
-}); 
+
 
 export default App;
